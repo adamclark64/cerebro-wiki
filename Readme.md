e44c0a120ef38150873a77e855289d07d173e415
adamclark64 <aclark64@gmail.com>
2017-03-08 18:14:57 -0700

    first

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# cerebro-duck-duck-go
+
+> [Cerebro](https://cerebroapp.com) plugin to use DuckDuckGo as a search engine
+
+## Usage
+
+In Cerebro, type any term, and the DuckDuckGo option will show above Google suggestions:
+
+![Sample](screenshot.png)
+
+## TODO
+
+- Preview Results as [Cerebro's native Google Plugin](https://github.com/KELiON/cerebro/blob/master/app/main/plugins/core/google/index.js)
+
+## License
+
+MIT © [Tiago Amaro](http://tiagoamaro.com.br)
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import icon from './icon.png'
+
+const order = 1
+
+const plugin = ({ term, actions, display }) => {
+  var search = (searchTerm) => {
+    const q = encodeURIComponent(searchTerm)
+    // https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&search=javascreipt&namespace=0&limit=10&suggest=true
+    // https://www.wikipedia.org/search-redirect.php?family=wikipedia&language=en&search=rice&language=en&go=Go
+    // console.log(':::::::::::::::::::', actions);
+    actions.open(' https://www.wikipedia.org/search-redirect.php?family=wikipedia&language=en&search=' + q + '&language=en&go=Go')
+    actions.hideWindow()
+  }
+
+  display({
+    icon: icon,
+    order: order, // High priority
+    title: `Search Wikipedia for ${term}`,
+    onSelect: () => search(term)
+  })
+}
+
+module.exports = {
+  fn: plugin
+}
