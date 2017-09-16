88b1e02bfc925e694f71447aabac9763107fb899
Demetrio <deme3@protonmail.com>
2017-09-16 21:24:39 +0200

    Improved preview style

--- src/Preview/index.js
+++ src/Preview/index.js
@@ -3,7 +3,7 @@ import styles from "./styles.css"
 
 module.exports = ({term, previewText, thumbnail}) => (
 	<div className={styles.main}>
-		<div className={styles.articleTitleBar}><h1>{term}<span>Wikipedia article</span></h1></div>
+		<div className={styles.articleTitleBar}><h1>{term}<span>From Wikipedia</span></h1></div>
 		<img className={styles.articleThumb} src={thumbnail} />
 		<p>{previewText}</p>
 	</div>
