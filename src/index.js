8c5427ba499e2761f7d7cc34f4a5920cdd62b705
Demetrio Battaglia <deme3@protonmail.com>
2017-09-16 21:33:26 +0200

    Remove dead comments

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ const previewCharBlacklist = ['\uf8ff', '\uFFFD']
 const plugin = ({ term, actions, display }) => {
   var search = (searchTerm) => {
     const q = encodeURIComponent(searchTerm)
-    // https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&search=javascreipt&namespace=0&limit=10&suggest=true
-    // https://www.wikipedia.org/search-redirect.php?family=wikipedia&language=en&search=rice&language=en&go=Go
-    // console.log(':::::::::::::::::::', actions);
     actions.open(' https://www.wikipedia.org/search-redirect.php?family=wikipedia&language=en&search=' + q + '&language=en&go=Go')
     actions.hideWindow()
   }
